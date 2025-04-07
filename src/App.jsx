2be53652bd93b582a2d71c1a60bcbda17a3baf24
Oloyede Israel <etzisrael912.ope@gmail.com>
2025-04-07 18:05:48 +0100

    Updated project files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@
  import { Navbar } from './components/Navbar';
  import {MobileMenu} from './components/MobileMenu';
  import { Home } from './components/sections/Home';
+ import { About } from './components/sections/About';
+ import { Projects } from './components/sections/Projects';
+ import { Contacts } from './components/sections/Contacts';
  import  "./index.css";
 
 
@@ -19,6 +22,9 @@ function App() {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen }/>
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen }/>
         <Home/>
+        <About/>
+        <Projects/>
+        <Contacts/>
       </div>
     </>
   );
